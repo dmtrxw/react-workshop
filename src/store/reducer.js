08ea24d5e194ed5c38d560726f98ae1b64b85df3
Dimitri Wahyudiputra <gitmitri@gmail.com>
2022-01-09 11:10:02 +0700

    fix: use constant on reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,11 @@
+import { SET_TODOS } from './constants'
+
 const initialState = {
     todos: [],
 }
 
 function reducer(state = initialState, action) {
-    if (action.type === 'SET_TODOS') {
+    if (action.type === SET_TODOS) {
         return { ...state, todos: action.payload }
     }
     return state
